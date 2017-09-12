d3cc2d66349d2bc0f9bf8575173aad5c5a9b1023
Gyles Batara and Hansen Hansen <hansen@>
2017-09-12 13:47:18 -0700

    Added function where woofs and meows disappear

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,17 @@ $(document).ready(function() {
   $("button#bark").click(function() {
     $("ul.dot1").prepend("<li>WOOF!</li>");
     $("ul.dot2").prepend("<li>MEOW!</li>")
+    $("li").click(function() {
+      $(this).remove();
   });
+  });
+
 
   $("button#meow").click(function() {
     $("ul.dot2").prepend("<li>MEOW!</li>");
     $("ul.dot1").prepend("<li>WOOF!</li>")
+    $("li").click(function() {
+      $(this).remove();
+  });
   });
 });
